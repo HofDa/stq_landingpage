754f59178cc2078c113ce6aa00ca17e9262cd348
Enigma1212 <meranomystery@gmail.com>
2025-09-08 11:32:40 +0200

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<stq_landingpage>/', // <= replace with your repo name
+  base: '/https://hofda.github.io/stq_landingpage//', // <= replace with your repo name
 })
